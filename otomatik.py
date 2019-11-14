d62a3c9c512d131b5c6b9108391fca269c340021
pcdunyasitv <36000164+pcdunyasitv@users.noreply.github.com>
2019-11-15 01:15:08 +0300

    Update otomatik.py

--- otomatik.py
+++ otomatik.py
@@ -6,10 +6,16 @@ import time
 
 os.system("anonsurf start")
 os.system("clear")
+print("Yeni IP Adres :")
+print("-----------------------------")
 os.system("curl icanhazip.com")
+print("-----------------------------")
 
 while True:
 	time.sleep(60)
 	os.system("anonsurf restart")
 	os.system("clear")
+	print("Yeni IP Adres :")
+	print("-----------------------------")
 	os.system("curl icanhazip.com")
+	print("-----------------------------")
